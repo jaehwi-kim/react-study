c9f920afd74b8508928f62e8376821239d595f3e
jkim589 <jkim589@illinois.edu>
2020-07-27 00:18:22 +0900

    exercise 2

---
+++ exercise_2/src/App.js
@@ -0,0 +1,13 @@
+import React, {  } from 'react';
+import MyComponent from './MyComponent'
+import './App.css'
+
+const App = () => {
+  return (
+    <MyComponent name = "React" favoriteNumber={1}>
+      리액트
+    </MyComponent>
+  )
+}
+
+export default App;
