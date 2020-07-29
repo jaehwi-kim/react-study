6df3970cb582f34cd7ffaf3985b99b7c6e7760c8
root <root@DESKTOP-ULV6LKK.localdomain>
2020-07-29 15:48:46 +0900

    commit before eject

--- exercise_20_(Server_side_rendering)/src/App.js
+++ exercise_20_(Server_side_rendering)/src/App.js
@@ -1,9 +1,18 @@
-import React, {  } from 'react';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Menu from './components/Menu';
+import RedPage from './pages/RedPage';
+import BluePage from './pages/BluePage';
 
 const App = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <Menu />
+      <hr />
+      <Route path="/red" component={RedPage} />
+      <Route path="/blue" component={BluePage} />
+    </div>
+  );
+};
 
 export default App;
