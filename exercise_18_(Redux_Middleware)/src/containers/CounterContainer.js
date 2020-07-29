4a36a11a93cf9eb7c73e640e55f9efb02e17e607
root <root@DESKTOP-ULV6LKK.localdomain>
2020-07-29 14:47:31 +0900

    exercise 18 middleware done

--- exercise_18_(Redux_Middleware)/src/containers/CounterContainer.js
+++ exercise_18_(Redux_Middleware)/src/containers/CounterContainer.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { increase, decrease } from '../modules/counter';
+// import { increase, decrease } from '../modules/counter';
 import Counter from '../components/Counter';
+import { increaseAsync, decreaseAsync } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
-  return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
-  );
-};
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
 
-export default connect(
-  state => ({
-    number: state.counter
-  }),
-  {
-    increase,
-    decrease
-  }
-)(CounterContainer);
+// redux thunk
+const CounterContainer = ({ number, increaseAsync, decreaseAsync }) => {
+    return (
+      <Counter
+        number={number}
+        onIncrease={increaseAsync}
+        onDecrease={decreaseAsync}
+      />
+    );
+  };
+  export default connect(
+    state => ({
+      number: state.counter
+    }),
+    {
+      increaseAsync,
+      decreaseAsync
+    }
+  )(CounterContainer);
+
+
+// export default connect(
+//   state => ({
+//     number: state.counter
+//   }),
+//   {
+//     increase,
+//     decrease
+//   }
+// )(CounterContainer);
--- exercise_18_(Redux_Middleware)/src/modules/index.js
+++ exercise_18_(Redux_Middleware)/src/modules/index.js
@@ -1,9 +1,23 @@
+// import { combineReducers } from 'redux';
+// import counter from './counter';
+
+// // create and export one reducer containing all reducers using combineReducers
+// const rootReducer = combineReducers({
+//   counter
+// });
+
+// export default rootReducer;
+
+// thunk api 처리
 import { combineReducers } from 'redux';
 import counter from './counter';
+import sample from './sample';
+import loading from './loading';
 
-// create and export one reducer containing all reducers using combineReducers
 const rootReducer = combineReducers({
-  counter
+  counter,
+  sample,
+  loading
 });
 
 export default rootReducer;
