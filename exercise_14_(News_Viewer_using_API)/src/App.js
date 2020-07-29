eacfb3dcb6fe3a7a1593435168616ef9dd9cc521
root <root@DESKTOP-ULV6LKK.localdomain>
2020-07-29 09:18:46 +0900

    exercise_14 done

--- exercise_14_(News_Viewer_using_API)/src/App.js
+++ exercise_14_(News_Viewer_using_API)/src/App.js
@@ -1,13 +1,15 @@
-import React, {  } from 'react';
-import Counter from './Counter'
-import Say from './Say'
-import './App.css'
+// 전체 뉴스 불러오기
+// GET https://newsapi.org/v2/top-headlines?country=kr&apiKey=e0f0b1309683476c8f211a09a27cefe2
 
+// 특정 카테고리 뉴스 불러오기
+// GET https://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey=e0f0b1309683476c8f211a09a27cefe2
+
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
+ 
 const App = () => {
-  return (
-    // <Counter/>
-    <Say></Say>
-  )
-}
-
+  return <Route path="/:category?" component={NewsPage} />;
+};
+ 
 export default App;
