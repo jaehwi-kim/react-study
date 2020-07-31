a7d34cc0c6b7b2f2eb94971c9a2fb6d6d9d0958e
root <root@DESKTOP-ULV6LKK.localdomain>
2020-07-31 14:17:51 +0900

    springReact project done

--- springReact/frontend/src/App.js
+++ springReact/frontend/src/App.js
@@ -1,29 +1,176 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import TodoListTemplate from './components/js/TodoListTemplate';
+import Form from './components/js/Form';
+import TodoItemList from './components/js/TodoItemList';
 
-const App = () => {
-  const [message, setMessage] = useState("");
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+         input : "",
+         todos : [
 
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(response => response.text())
-      .then(message => {
-        setMessage(message);
+          ]
+      }
+      this.handleChange = this.handleChange.bind(this);
+      this.handleCreate = this.handleCreate.bind(this);
+      this.handleKeyPress = this.handleKeyPress.bind(this);
+      this.handleToggle = this.handleToggle.bind(this);
+      this.handleRemove = this.handleRemove.bind(this);
+      this.handleInitInfo = this.handleInitInfo.bind(this);
+  }
+
+  componentDidMount() {
+      fetch("/api/todos")
+          .then(res => res.json())
+          .then(todos => this.setState({todos : todos}))
+          .catch(err => console.log(err))
+  }
+
+  handleInitInfo() {
+      fetch("/api/todos")
+          .then(res => res.json())
+          .then(todos => this.setState({todos : todos}))
+          .catch(err => console.log(err))
+  }
+
+  // input 값 변경
+  handleChange(event) {
+      this.setState({
+          input: event.target.value
       });
-  }, [])
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title"> {message} </h1>
-      </header>
-      <p className="App-intro">
-        asdfasdfasdfasdf
-      </p>
-    </div>
-  );
+  // 등록
+  handleCreate() {
+      const { input, todos } = this.state;
+      if (input === "") {
+          alert("오늘 할 일을 입력해주세요!");
+          return;
+      }
+
+      // 화면에서 먼저 변경사항을 보여주는 방법으로 이용
+      this.setState({
+          input: "",
+          // concat 을 사용하여 배열에 추가
+          todos: todos.concat({
+              id: 0,    // 임의의 id를 부여하여 key error 를 방지
+              content: input,
+              isComplete: false
+          })
+      });
+
+      // 처리
+      const data = {
+          body: JSON.stringify({"content" : input}),
+          headers: {'Content-Type': 'application/json'},
+          method: 'post'
+      }
+      fetch("/api/todos", data)
+          .then(res => {
+              if(!res.ok) {
+                  throw new Error(res.status);
+              } else {
+                  return this.handleInitInfo();
+              }
+          })
+          .catch(err => console.log(err));
+  }
+
+  // Enter Key 이벤트
+  handleKeyPress(event) {
+      if (event.key === "Enter") {
+          this.handleCreate();
+      }
+  }
+
+  // 수정
+  handleToggle(id) {
+      const todos = this.state.todos;
+
+      const isComplete = todos.find(todo => todo.id === id).isComplete;
+      if(!window.confirm(isComplete ? "미완료 처리 하시겠습니까?" : "완료 처리 하시겠습니까?")) {
+          return;
+      }
+
+      // 파라미터로 받은 id 를 가지고 몇 번째 아이템인지 찾는다.
+      const index = todos.findIndex(todo => todo.id === id);
+
+      // 선택한 객체를 저장한다.
+      const selected = todos[index];
+
+      // 배열을 복사한다.
+      const nextTodos = [...todos];
+
+      // 기존의 값을 복사하고 isComplete 값을 덮어쓴다.
+      nextTodos[index] = {
+          ...selected,
+          isComplete : !selected.isComplete
+      };
+
+      this.setState({
+          todos : nextTodos
+      });
+
+      const data = {
+          headers: {'Content-Type':'application/json'},
+          method: 'put'
+      }
+      fetch("/api/todos/" + id, data)
+      .then(res => {
+          if(!res.ok) {
+              throw new Error(res.status);
+          } else {
+              return this.handleInitInfo();
+          }
+      })
+      .catch(err => console.log(err));
+  }
+
+  // 삭제
+  handleRemove(id) {
+      const todos = this.state.todos;
+
+      const removeContent = todos.find(todo => todo.id === id).content;
+      if(!window.confirm("'" + removeContent + "' 을 삭제하시겠습니까?")) {
+          return;
+      }
+
+      this.setState({
+          todos : todos.filter(todo => todo.id !== id)
+      });
+
+      const data = {
+          headers: {'Content-Type':'application/json'},
+          method: 'delete'
+      }
+      fetch("/api/todos/" + id, data)
+      .then(res => {
+          if(!res.ok) {
+              throw new Error(res.status);
+          } else {
+              return this.handleInitInfo();
+          }
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+      return (
+          <TodoListTemplate form={(
+              <Form
+                  value={this.state.input}
+                  onChange={this.handleChange}
+                  onCreate={this.handleCreate}
+                  onKeyPress={this.handleKeyPress} />
+          )}>
+              <TodoItemList
+                  todos={this.state.todos}
+                  onToggle={this.handleToggle}
+                  onRemove={this.handleRemove} />
+          </TodoListTemplate>
+      );
+  }
 }
 
 export default App;
--- springReact/src/main/java/test/springReact/todolist/controller/TodoController.java
+++ springReact/src/main/java/test/springReact/todolist/controller/TodoController.java
@@ -1,5 +1,7 @@
 package test.springReact.todolist.controller;
-
+// controller works as a gateway between input and domain logic
+// decides what to do with the input and how to output the response
+// 사용자의 요청을 어떻게 처리할지 결정
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -8,14 +10,17 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+// RestController  반환값은 모두 Object타입이지만 상태코드, 응답 메시지등이 API로 반환될때 사용
+// 상태 코드를 필수적으로 리턴
+// 결과적으로 Clinet요청에 대한 응답을 한번 더 감싸며 상태 코드에 따라 다른 화면을 노출시킬 수도 있고 Header값에 따라 다른 동작 가능
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping; // maps http delete requests onto specific handler methods
+import org.springframework.web.bind.annotation.GetMapping; // maps http get requests onto specific handler methods
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping; // maps http post requests onto specific handler methods
+import org.springframework.web.bind.annotation.PutMapping; // maps http put requests onto specific handler methods
+import org.springframework.web.bind.annotation.RequestBody; // http request를 가바 객체로 전달받음
+import org.springframework.web.bind.annotation.RequestMapping; // maps HTTP requests to handler methods of controllers
 import org.springframework.web.bind.annotation.RestController;
 
 import test.springReact.todolist.domain.Todo;
